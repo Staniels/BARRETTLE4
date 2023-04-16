3c852f5694ea6f0bbf2273f1900cdf82e44302bf
Staniels <kuyamata50@gmail.com>
2023-04-16 21:58:59 +0800

    BARRETTOLE4

---
+++ BARRETTOLE2/BARRETTOBlogDB/Tables/Post.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Post]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [Title] NVARCHAR(150) NOT NULL, 
+    [Body] TEXT NOT NULL, 
+    [DateCreated] DATETIME2 NOT NULL, 
+    CONSTRAINT [FK_Post_Users] FOREIGN KEY (UserId) REFERENCES Users(Id) 
+)
